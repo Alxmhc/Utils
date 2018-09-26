a83a961019d026b497e2bf1e33a09266102e2864
Alxmhc <Alxmhc@gmail.com>
2018-09-26 15:54:11 +0300

    range extraction

--- JavaScript/_server/fls.js
+++ JavaScript/_server/fls.js
@@ -44,6 +44,30 @@ function _dir_fls(dir, fn, depth, res, onDone) {
 	})
 }
 
+function get_range(req){
+	let range = req.headers.range;
+	if(!range)
+		return null;
+	const str = 'bytes=';
+	range = range.slice(str.length);
+	return range.split('-').map(e => parseInt(e, 10));
+}
+
+function calc_range(range, size){
+	let start = range[0], end = range[1];
+	if(isNaN(start)){
+		start = end < size ? size - end : 0;
+		end = size - 1;
+	} else {
+		if(isNaN(end) || end >= size){
+			end = size - 1;
+		}
+		if(start > end)
+			return null;
+	}
+	return [start, end];
+}
+
 function send_file(path, type, req, res){
 	fs.stat(path, (err, st) => {
 		if(err){
@@ -51,30 +75,23 @@ function send_file(path, type, req, res){
 			res.end()
 			return
 		}
-		const range = req.headers.range;
+		let range = get_range(req);
 		if(!range){
 			res.writeHead(200, {'Content-Type': type})
 			fs.createReadStream(path).pipe(res);
 			return
 		}
-		const sz = st.size
-		const str = 'bytes='
-		const arr = range.slice(str.length).split('-')
-		let start, end
-		if(arr[0]){
-			start = parseInt(arr[0], 10)
-			end = arr[1] ? parseInt(arr[1], 10) : 0
-			if(!end || end>=sz)
-				end = sz - 1
+		const sz = st.size;
+		range = calc_range(range, sz);
+		if(!range){
+			res.writeHead(416, {'Content-Range': 'bytes */' + sz});
+			res.end();
+			return;
 		}
-		else{
-			start = sz - parseInt(arr[1], 10)
-			end = sz - 1
-		}
-		res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + sz,
-		                     'Content-Length': (end - start) + 1,
+		res.writeHead(206, { 'Content-Range': 'bytes ' + range[0] + '-' + range[1] + '/' + sz,
+		                     'Content-Length': (range[1] - range[0]) + 1,
 		                     'Content-Type': type });
-		fs.createReadStream(path, {start: start, end: end}).pipe(res);
+		fs.createReadStream(path, {start: range[0], end: range[1]}).pipe(res);
 	})
 }
 
