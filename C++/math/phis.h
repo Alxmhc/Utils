b0bd8aae11101ff11d0e830ef2ca66abf9dfbf6f
Alxmhc <Alxmhc@gmail.com>
2018-11-23 23:19:17 +0300

    restructuring

--- C++/math/phis.h
+++ C++/math/phis.h
@@ -3,12 +3,15 @@ long double Ek(const std::vector<long double> &m, const std::vector<T> &v)
 {
 	long double s = 0;
 	auto n = m.size();
-	while(n--){s += m[n]*(v[n]*v[n]);}
+	while(n--)
+	{
+		s += m[n] * (v[n]*v[n]);
+	}
 	return 0.5*s;
 }
 
 template<typename T>
 long double Ek(long double m, const std::vector<T> &v)
 {
-	return 0.5*m*(v*v);
+	return 0.5*m * (v*v);
 }
