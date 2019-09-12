4ab7350b259b931383b480f99f684411f611679b
Alxmhc <Alxmhc@gmail.com>
2019-09-12 08:45:44 +0300

    ?

--- JavaScript/_server/db/SQL.js
+++ JavaScript/_server/db/SQL.js
@@ -1,16 +1,15 @@
-let SQL = {}
-SQL.insert = function(t, vl, nm = null){
-	let s = 'INSERT INTO '+t;
-	if(nm){
-		s += '('+nm.join(',')+')';
+module.exports =  {
+	insert: function(t, vl, nm = null){
+		let s = 'INSERT INTO '+t;
+		if(nm){
+			s += '('+nm.join(',')+')';
+		}
+		return s+' VALUES('+vl.map(e=>typeof e!='string'?e:'"'+e+'"').join(',')+')';
+	},
+	select: function(t, nm, ext = ''){
+		return 'SELECT '+nm.join(',')+' FROM '+t+ext;
+	},
+	delete: function(t, ext){
+		return 'DELETE FROM '+t+' WHERE '+ext;
 	}
-	return s+' VALUES('+vl.map(e=>typeof e!='string'?e:'"'+e+'"').join(',')+')';
 }
-SQL.select = function(t, nm, ext = ''){
-	return 'SELECT '+nm.join(',')+' FROM '+t+ext;
-}
-SQL.delete = function(t, ext){
-	return 'DELETE FROM '+t+' WHERE '+ext;
-}
-
-module.exports = SQL
--- JavaScript/_server/db/sqlite.js
+++ JavaScript/_server/db/sqlite.js
@@ -1,42 +1,41 @@
 //npm sqlite3
 const SQL = require('./SQL.js');
 
-const sqlite = {}
-sqlite.select_all = function(db, t, nm, ext=''){
-	const q = SQL.select(t, nm, ext);
-	return new Promise((res, rej) => {
-		db.all(q, function(err, rs){
-			if(err){
-				rej(err);
-				return;
-			}
-			res(rs);
+module.exports = {
+	select_all: function(db, t, nm, ext=''){
+		const q = SQL.select(t, nm, ext);
+		return new Promise((res, rej) => {
+			db.all(q, function(err, rs){
+				if(err){
+					rej(err);
+					return;
+				}
+				res(rs);
+			})
 		})
-	})
-}
-sqlite.insert = function(db, t, vl, nm = null){
-	const q = SQL.insert(t, vl, nm);
-	return new Promise((res, rej) => {
-		db.run(q, function(err){
-			if(err){
-				rej(err);
-				return;
-			}
-			res(this.lastID);
+	},
+	insert: function(db, t, vl, nm = null){
+		const q = SQL.insert(t, vl, nm);
+		return new Promise((res, rej) => {
+			db.run(q, function(err){
+				if(err){
+					rej(err);
+					return;
+				}
+				res(this.lastID);
+			})
 		})
-	})
+	},
+	delete: function(db, t, ext){
+		const q = SQL.delete(t, ext);
+		return new Promise((res, rej) => {
+			db.run(q, function(err){
+				if(err){
+					rej(err);
+					return;
+				}
+				res();
+			})
+		})	
+	}
 }
-sqlite.delete = function(db, t, ext){
-	const q = SQL.delete(t, ext);
-	return new Promise((res, rej) => {
-		db.run(q, function(err){
-			if(err){
-				rej(err);
-				return;
-			}
-			res();
-		})
-	})	
-}
-
-module.exports = sqlite
