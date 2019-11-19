0651bab05cae37fedc884165f0240379f96fb027
Alxmhc <Alxmhc@gmail.com>
2019-11-19 19:26:30 +0300

    js game

--- JavaScript/_client/gr/gm.js
+++ JavaScript/_client/gr/gm.js
@@ -1,19 +1,30 @@
-function game_field(fld, im){
-	this.f = fld;
-	this.im = im.slice();
-}
-game_field.prototype = {
-	crd: function(cr){
-		return this.f.crd_e(cr);
-	},
-	clear: function(){
+class game_field{
+	constructor(fld, im, rev = false){
+		this.f = fld;
+		this.im = im.slice();
+		this._rev = rev;
+	}
+	crd(cr){
+		let c = this.f.crd_e(cr);
+		this.corr_cr(c);
+		return c;
+	}
+	corr_cr(cr){
+		if(this._rev){
+			cr[0] = this.f.sx - 1 - cr[0];
+			cr[1] = this.f.sy - 1 - cr[1];
+		}
+	}
+
+	clear(){
 		this.f.clear();
-	},
-	draw: function(cr, n){
+	}
+	draw(cr, n){
+		this.corr_cr(cr);
 		if(n === null){
 			this.f.del(cr);
 			return;
-		}		
+		}
 		const o = this.im[n];
 		if(o instanceof HTMLImageElement){
 			this.f.del(cr);
@@ -22,21 +33,12 @@ game_field.prototype = {
 		else if(typeof o === "string"){
 			this.f.fll(cr, o);
 		}
-	},
-
-	set_pos: function(p){
-		this.clear();
-		for(let k in p){
-			for(let cr of p[k]){
-				this.draw(cr, k);
-			}
-		}
-	},
-	set_arr: function(arr, sz){
+	}
+	set_arr(arr){
 		this.clear();
 		let k = 0;
-		for(let y = 0; y < sz[1]; ++y){
-			for(let x = 0; x < sz[0]; ++x){
+		for(let y = 0; y < this.f.sy; ++y){
+			for(let x = 0; x < this.f.sx; ++x){
 				const c = arr[k];
 				k++;
 				if(c === null)
@@ -44,9 +46,18 @@ game_field.prototype = {
 				this.draw([x,y], c);
 			}
 		}
-	},
+	}
+
+	set_pos(p){
+		this.clear();
+		for(let k in p){
+			for(let cr of p[k]){
+				this.draw(cr, k);
+			}
+		}
+	}
 	//m - [[cr,o],...]
-	moves: function(m){
+	moves(m){
 		for(let v of m){
 			this.draw(v[0], v[1]);
 		}
--- JavaScript/game/chess.js
+++ JavaScript/game/chess.js
@@ -1,87 +1,87 @@
-Chess = {}
-Chess.sz = [8,8];
-Chess.color = ['white', 'black'];
-Chess.type = ['pawn', 'rook', 'knight', 'bishop', 'queen', 'king'];
+Chess = {
+	sz: [8,8],
+	color: ['white', 'black'],
+	type: ['pawn', 'rook', 'knight', 'bishop', 'queen', 'king']
+}
 
 Chess.inf = function(p){
 	if(p === null)
-		return null;
+		return {color: null, type: null};
 	if(p < 6)
 		return {color: 0, type: p};
 	return {color: 1, type: p - 6};
 }
 
-Chess.fen = 'PRNBQKprnbqk'
-
-Chess.fen_to_pos = function(s){
-	let res = {}, x=0, y=0;
-	for(let i=0, l=s.length; i<l; ++i){
-		const c = s[i];
-		if(c === '/'){
-			y++;
-			x = 0;
-			continue;
-		}
-		const e = Chess.fen.indexOf(c);
-		if(e === -1){
-			x += parseInt(c, 10);
-			continue;
-		}
-		const cr = [x,y];
-		if(res[e] === undefined){
-			res[e] = [cr];
-		}
-		else{
-			res[e].push(cr);
-		}
-		x++;
-	}
-	return res;
-}
-
-Chess.fen_to_arr = function(s){
-	let res = new Array(Chess.sz[0]*Chess.sz[1]);
-	res.fill(null);
-	let k = 0;
-	for(let i=0, l=s.length; i<l; ++i){
-		const c = s[i];
-		if(c === '/')
-			continue;
-		const e = Chess.fen.indexOf(c);
-		if(e === -1){
-			k += parseInt(c, 10);
-			continue;
-		}
-		res[k] = e;
-		k++;
-	}
-	return res;
-}
-
-Chess.arr_to_fen = function(arr){
-	const sx = Chess.sz[0], sy = Chess.sz[1];
-	let res = "", k = 0;
-	for(let y=0; y<sy; ++y){
-		if(y != 0){
-			res += '/';
-		}
-		let t = 0;
-		for(let x=0; x<sx; ++x){
-			const c = arr[k];
-			k++;
-			if(c === null){
-				t++;
+Chess.fen = {
+	_dict: 'PRNBQKprnbqk',
+	to_pos: function(s){
+		let res = {}, x=0, y=0;
+		for(let i=0, l=s.length; i<l; ++i){
+			const c = s[i];
+			if(c === '/'){
+				y++;
+				x = 0;
 				continue;
+			}
+			const e = this._dict.indexOf(c);
+			if(e === -1){
+				x += parseInt(c, 10);
+				continue;
+			}
+			const cr = [x,y];
+			if(res[e] === undefined){
+				res[e] = [cr];
+			}
+			else{
+				res[e].push(cr);
+			}
+			x++;
+		}
+		return res;
+	},
+	to_arr: function(s){
+		let res = new Array(Chess.sz[0]*Chess.sz[1]);
+		res.fill(null);
+		let k = 0;
+		for(let i=0, l=s.length; i<l; ++i){
+			const c = s[i];
+			if(c === '/')
+				continue;
+			const e = this._dict.indexOf(c);
+			if(e === -1){
+				k += parseInt(c, 10);
+				continue;
+			}
+			res[k] = e;
+			k++;
+		}
+		return res;
+	},
+	from_arr: function(arr){
+		const sx = Chess.sz[0], sy = Chess.sz[1];
+		let res = "", k = 0;
+		for(let y=0; y<sy; ++y){
+			if(y != 0){
+				res += '/';
+			}
+			let t = 0;
+			for(let x=0; x<sx; ++x){
+				const c = arr[k];
+				k++;
+				if(c === null){
+					t++;
+					continue;
+				}
+				if(t != 0){
+					res += t;
+					t = 0;
+				}
+				res += this._dict[c];
 			}
 			if(t != 0){
 				res += t;
-				t = 0;
 			}
-			res += Chess.fen[c];
 		}
-		if(t != 0){
-			res += t;
-		}
+		return res;
 	}
-	return res;
 }
