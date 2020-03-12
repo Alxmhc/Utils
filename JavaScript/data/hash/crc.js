a205d5c52c74579fc99382a2d4a9951251b1febd
Alxmhc <Alxmhc@gmail.com>
2020-03-12 22:01:10 +0300

    crc

---
+++ JavaScript/data/hash/crc.js
@@ -0,0 +1,28 @@
+class crc32_rfc1952{
+	constructor(){
+		this.crc = 0xffffffff;
+		this.tbl = new Uint32Array(256);
+		for(let n = 0; n < 256; n++){
+			let c = n;
+			for(let k = 0; k < 8; k++){
+				if((c & 1) != 0){
+					c = (c >>> 1) ^ 0xedb88320;
+				}
+				else{
+					c >>>= 1;
+				}
+			}
+			this.tbl[n] = c;
+		}
+	}
+	Update(v){
+		for(let i = 0; i < v.length; i++){
+			this.crc = this.tbl[(this.crc & 0xff) ^ v[i]] ^ (this.crc >>> 8);
+		}
+	}
+	Final(){
+		const res = this.crc ^ 0xffffffff;
+		this.crc = 0xffffffff;
+		return res >>> 0;
+	}
+}
