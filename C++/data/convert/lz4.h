080fd252934e070fd4ee59f5d58691d22da37a68
Alxmhc <Alxmhc@gmail.com>
2020-05-28 18:09:01 +0300

    jsonlz4

--- C++/data/convert/lz4.h
+++ C++/data/convert/lz4.h
@@ -4,42 +4,42 @@ namespace convert
 	{
 		class Decoder
 		{
-		    static void add_size(byteReader &br, std::size_t &sz)
-		    {
-		        if(sz != 15)
-                    return;
-		        for(;;)
-                {
-                    uint8_t b;
-                    br.get(b);
-                    sz += b;
-                    if(b != 255)
-                        break;
-                }
-		    }
+			static void add_size(byteReader &br, std::size_t &sz)
+			{
+				if(sz != 15)
+					return;
+				for(;;)
+				{
+					uint8_t b;
+					br.get(b);
+					sz += b;
+					if(b != 255)
+						break;
+				}
+			}
 		public:
 			Decoder() {}
 
-			std::vector<uint8_t> Decode_block(byteReader &br, std::size_t sz = 0) const
+			static std::vector<uint8_t> Decode_block(byteReader &br, std::size_t sz = 0)
 			{
-                std::vector<uint8_t> out;
-                out.reserve(sz);
+				std::vector<uint8_t> out;
+				out.reserve(sz);
 
-                uint8_t b;
-                while(br.get(b))
-                {
-                    std::size_t size = b >> 4;
-                    std::size_t len = b & 15;
-                    add_size(br, size);
-                    if(br.read(out, size) < size)
-                        break;
-                    uint16_t offset;
-                    if(!br.getC<endianness::LITTLE_ENDIAN>(offset))
-                        break;
-                    add_size(br, len);
-                    len += 4;
-                    LZ77_repeat(len, offset, out);
-                }
+				uint8_t b;
+				while(br.get(b))
+				{
+					std::size_t size = b >> 4;
+					std::size_t len = b & 15;
+					add_size(br, size);
+					if(br.read(out, size) < size)
+						break;
+					uint16_t offset;
+					if(!br.getC<endianness::LITTLE_ENDIAN>(offset))
+						break;
+					add_size(br, len);
+					len += 4;
+					LZ77_repeat(len, offset, out);
+				}
 				return out;
 			}
 		};
---
+++ C++/fl/jsonlz4.h
@@ -0,0 +1,29 @@
+namespace fl_pr
+{
+	namespace jsonlz4
+	{
+		const uint_fast8_t offset = 12;
+		uint32_t read_size(byteReader &s)
+		{
+			{
+				uint8_t hdr[8];
+				s.read(hdr, 8);
+				if(std::memcmp(hdr, "\x6d\x6f\x7a\x4c\x7a\x34\x30\x00", 8) != 0)
+					return 0;
+			}
+			uint32_t fsize = 0;
+			s.getC<endianness::LITTLE_ENDIAN>(fsize);
+			return fsize;
+		}
+		void Unpack(const char* f_in, const char* f_out)
+		{
+			std::ifstream fl(f_in, std::ios_base::binary);
+			br_stream br(fl);
+			auto size = read_size(br);
+			if(size == 0)
+				return;
+			auto data = convert::lz4::Decoder::Decode_block(br, size);
+			fwt(f_out, data.data(), data.size());
+		}
+	}
+}
