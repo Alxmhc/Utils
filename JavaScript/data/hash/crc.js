eb95a0c0f731818b63270b46e56cc545e4f93f58
Alxmhc <Alxmhc@gmail.com>
2021-04-15 00:22:10 +0300

    crc

--- JavaScript/data/hash/crc.js
+++ JavaScript/data/hash/crc.js
@@ -2,19 +2,7 @@
 class crc32{
 	constructor(){
 		this.crc = 0xffffffff;
-		this.tbl = new Uint32Array(256);
-		for(let n = 0; n < 256; n++){
-			let c = n;
-			for(let k = 0; k < 8; k++){
-				if((c & 1) != 0){
-					c = (c >>> 1) ^ 0xedb88320;
-				}
-				else{
-					c >>>= 1;
-				}
-			}
-			this.tbl[n] = c;
-		}
+		this.tbl = crc32.getTable(0xedb88320);
 	}
 	Update(v){
 		for(let i = 0; i < v.length; i++){
@@ -26,4 +14,20 @@ class crc32{
 		this.crc = 0xffffffff;
 		return res >>> 0;
 	}
+}
+crc32.getTable = function(I){
+	let tbl = new Uint32Array(256);
+	for(let n = 0; n < 256; n++){
+		let c = n;
+		for(let k = 0; k < 8; k++){
+			if((c & 1) != 0){
+				c = (c >>> 1) ^ I;
+			}
+			else{
+				c >>>= 1;
+			}
+		}
+		tbl[n] = c;
+	}
+	return tbl;
 }
--- Python/_win/reg.py
+++
@@ -1,7 +0,0 @@
-import winreg
-
-def read(hkey, path, name):
-	k = winreg.OpenKey(getattr(winreg,hkey), path)
-	res = winreg.QueryValueEx(k, name)
-	winreg.CloseKey(k)
-	return res[0]
