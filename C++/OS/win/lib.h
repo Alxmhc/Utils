ee66edebdeb42053e30c8dae7fdf3cb85782531d
Alxmhc <Alxmhc@gmail.com>
2021-08-12 23:33:59 +0300

    -

--- C++/OS/win/lib.h
+++ C++/OS/win/lib.h
@@ -1,30 +1,30 @@
-class dlib
-{
-	HINSTANCE inst;
-public:
-	dlib() : inst(nullptr) {}
-	~dlib()
-	{
-		free();
-	}
-
-	void load(const char *pth)
-	{
-		inst = LoadLibrary_(pth);
-	}
-	void free()
-	{
-		if(inst != nullptr)
-		{
-			FreeLibrary(inst);
-			inst = nullptr;
-		}
-	}
-
-	void* get_addr(const char *fname) const
-	{
-		if(inst == nullptr)
-			return nullptr;
-		return GetProcAddress(inst, fname);
-	}
-};
+class dlib
+{
+	HINSTANCE inst;
+public:
+	dlib() : inst(nullptr) {}
+	~dlib()
+	{
+		free();
+	}
+
+	void load(const char *pth)
+	{
+		inst = LoadLibrary_(pth);
+	}
+	void free()
+	{
+		if(inst != nullptr)
+		{
+			FreeLibrary(inst);
+			inst = nullptr;
+		}
+	}
+
+	void* get_addr(const char *fname) const
+	{
+		if(inst == nullptr)
+			return nullptr;
+		return GetProcAddress(inst, fname);
+	}
+};
