2e9c697e0c4b7f8216db1b66c3041a7b7f2d9729
Alxmhc <Alxmhc@gmail.com>
2021-09-15 14:21:14 +0300

    ppm

---
+++ Go/fls/parsefile/fl_ppm/ppm.go
@@ -0,0 +1,22 @@
+package fl_ppm
+
+import (
+	"image"
+	"image/color"
+	"io"
+)
+
+func ReadImgRGB(dat io.Reader, szx, szy int) image.Image {
+	img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{szx, szy}})
+	c := make([]byte, 3)
+	for y := 0; y < szy; y++ {
+		for x := 0; x < szx; x++ {
+			n, _ := dat.Read(c)
+			if n != 3 {
+				return nil
+			}
+			img.SetRGBA(x, y, color.RGBA{c[0], c[1], c[2], 0xff})
+		}
+	}
+	return img
+}
