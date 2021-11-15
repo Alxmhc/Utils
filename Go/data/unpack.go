08035a699c498636bf562976cf01bb77e7452d39
Alxmhc <Alxmhc@gmail.com>
2021-11-15 08:58:22 +0300

    unpack

--- Go/data/unpack.go
+++ Go/data/unpack.go
@@ -1,18 +1,17 @@
 package data
 
-func LZ77_repeat(sz, dist int, d []byte) []byte {
-	osz := len(d)
-	if dist == 0 || osz < dist {
+func LZ77_repeat(sz, dist uint, d []byte) []byte {
+	if dist == 0 || dist > uint(len(d)) {
 		return nil
 	}
-	beg := osz - dist
+	if sz == 0 {
+		return d
+	}
+	beg := uint(len(d)) - dist
 	for sz > dist {
 		d = append(d, d[beg:]...)
 		sz -= dist
 		dist += dist
-	}
-	if sz == 0 {
-		return d
 	}
 	return append(d, d[beg:beg+sz]...)
 }
