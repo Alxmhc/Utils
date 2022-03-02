6bd8d9a18d70e48f7851f59e8d25383535f9d0d9
Alxmhc <Alxmhc@gmail.com>
2022-03-02 19:20:14 +0300

    -p

--- C++/fl/jsonlz4.h
+++ C++/fl/jsonlz4.h
@@ -5,13 +5,12 @@ namespace fl_pr
 		const uint_fast8_t offset = 12;
 		bool read_size(byteReader &s, uint_fast32_t &fsz)
 		{
-			uint8_t hdr[8];
-			if(!s.readN(hdr, 8))
+			uint8_t hdr[12];
+			if(!s.readN(hdr, 12))
 				return false;
 			if(std::memcmp(hdr, "\x6d\x6f\x7a\x4c\x7a\x34\x30\x00", 8) != 0)
 				return false;
-			if (!s.readC<4, endianness::LITTLE_ENDIAN>(fsz))
-				return false;
+			fsz = bconv<4, endianness::LITTLE_ENDIAN>::pack(hdr + 8);
 			return true;
 		}
 		bool Unpack(byteReader &br, std::vector<uint8_t> &out)
--- Python/SQL/SQL.py
+++
@@ -1,9 +0,0 @@
-def select(t, cl, ext=''):
-	return 'SELECT '+','.join(cl)+' FROM '+t+ext
-
-def insert(t, vl, cl = None):
-	s = 'INSERT INTO '+t
-	if(not cl is None):
-		s += '('+','.join(cl)+')'
-	v = map(lambda e: '"'+e+'"' if (type(e) is str) else str(e), vl)
-	return s+' VALUES('+','.join(v)+')'
