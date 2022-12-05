84130c2173ce34c548824d8c2994fb46742e9985
Alxmhc <Alxmhc@gmail.com>
2022-12-05 21:12:26 +0300

    fls

--- C++/OS/win/fls.h
+++ C++/OS/win/fls.h
@@ -1,91 +1,134 @@
-class fl_s
+namespace fl_s
 {
-	static bool is_n(const std::string &s){return s == "." || s == "..";}
-	static bool is_n(const std::wstring &s){return s == L"." || s == L"..";}
-
-	template<typename S>
-	static void dir_ex(const S &d, std::vector<S> &r, bool (*filter)(const S&), int depth, bool is_dir)
+	class list
 	{
-		S n = d;
-		n.push_back('*');
-		typename WIN32_FIND_DATA_<S>::T ffd;
-		HANDLE hf = FindFirstFile_(n.c_str(), &ffd);
-		if (hf == INVALID_HANDLE_VALUE)
-			return;
-		do {
-			n = ffd.cFileName;
-			S pth = d + n;
-			if(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-			{
-				if(is_n(n))
-					continue;
-				if(is_dir && filter(pth))
+		template<typename S> struct WIN32_FIND_DATA_{};
+		template<> struct WIN32_FIND_DATA_<char>{typedef WIN32_FIND_DATAA T;};
+		template<> struct WIN32_FIND_DATA_<std::string>{typedef WIN32_FIND_DATAA T;};
+		template<> struct WIN32_FIND_DATA_<wchar_t>{typedef WIN32_FIND_DATAW T;};
+		template<> struct WIN32_FIND_DATA_<std::wstring>{typedef WIN32_FIND_DATAW T;};
+
+		static HANDLE FindFirstFile_(LPCSTR filename, LPWIN32_FIND_DATAA ffd)
+		{
+			return FindFirstFileA(filename, ffd);
+		}
+		static HANDLE FindFirstFile_(LPCWSTR filename, LPWIN32_FIND_DATAW ffd)
+		{
+			return FindFirstFileW(filename, ffd);
+		}
+		static bool FindNextFile_(HANDLE hff, LPWIN32_FIND_DATAA ffd)
+		{
+			return FindNextFileA(hff, ffd) == TRUE;
+		}
+		static bool FindNextFile_(HANDLE hff, LPWIN32_FIND_DATAW ffd)
+		{
+			return FindNextFileW(hff, ffd) == TRUE;
+		}
+
+		template<typename S>
+		static void dir_ex(const S &d, std::vector<S> &r, bool (*filter)(const S&), int depth, bool is_dir)
+		{
+			S pth = d;
+			pth.push_back('*');
+			typename WIN32_FIND_DATA_<S>::T ffd;
+			HANDLE hf = FindFirstFile_(pth.c_str(), &ffd);
+			if (hf == INVALID_HANDLE_VALUE)
+				return;
+			do {
+				if(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 				{
-					r.push_back(pth);
+					if(ffd.cFileName[0] == '.')
+					{
+						if(ffd.cFileName[1] == 0)
+							continue;
+						if(ffd.cFileName[2] == 0
+						&& ffd.cFileName[1] == '.')
+							continue;
+					}
+					pth = d + ffd.cFileName;
+					if(is_dir && filter(pth))
+					{
+						r.push_back(pth);
+					}
+					if(depth != 0)
+					{
+						pth.push_back('/');
+						dir_ex(pth, r, filter, depth - 1, is_dir);
+					}
 				}
-				if(depth != 0)
+				else if(!is_dir)
 				{
-					pth.push_back('/');
-					dir_ex(pth, r, filter, depth - 1, is_dir);
+					pth = d + ffd.cFileName;
+					if(filter(pth))
+					{
+						r.push_back(pth);
+					}
 				}
-			}
-			else if(!is_dir && filter(pth))
-			{
-				r.push_back(pth);
-			}
-		} while ( FindNextFile_(hf, &ffd) );
-		FindClose(hf);
+			} while ( FindNextFile_(hf, &ffd) );
+			FindClose(hf);
+		}
+	public:
+		template<typename C>
+		static std::vector<std::basic_string<C>> dir_files(const C* d, bool (*filter)(const std::basic_string<C>&), int depth = -1)
+		{
+			std::basic_string<C> p(d);
+			p.push_back('/');
+			std::vector<std::basic_string<C>> r;
+			dir_ex(p, r, filter, depth, false);
+			return r;
+		}
+
+		template<typename C>
+		static std::vector<std::basic_string<C>> dir_folders(const C* d, bool (*filter)(const std::basic_string<C>&), int depth = -1)
+		{
+			std::basic_string<C> p(d);
+			p.push_back('/');
+			std::vector<std::basic_string<C>> r;
+			dir_ex(p, r, filter, depth, true);
+			return r;
+		}
+	};
+
+	bool del_file(const char* filename)
+	{
+		return DeleteFileA(filename) == TRUE;
 	}
-public:
+	bool del_file(const wchar_t* filename)
+	{
+		return DeleteFileW(filename) == TRUE;
+	}
+
+	bool del_dir(const char* dirname)
+	{
+		return RemoveDirectoryA(dirname) == TRUE;
+	}
+	bool del_dir(const wchar_t* dirname)
+	{
+		return RemoveDirectoryW(dirname) == TRUE;
+	}
+
+	bool create_dir(const char* pth)
+	{
+		return CreateDirectoryA(pth, NULL) == TRUE;
+	}
+	bool create_dir(const wchar_t* pth)
+	{
+		return CreateDirectoryW(pth, NULL) == TRUE;
+	}
 
 	template<typename C>
-	static std::vector<std::basic_string<C>> dir_files(const C* d, bool (*filter)(const std::basic_string<C>&), int depth = -1)
+	bool create_dirs(const C* pth)
 	{
-		std::basic_string<C> p(d);
-		if(p.back() != '/')
+		std::basic_string<C> t(pth);
+		for(size_t i = 0; i < t.length(); i++)
 		{
-			p.push_back('/');
+			if(t[i] == '/' || t[i] == '\\')
+			{
+				t[i] = 0;
+				create_dir(t.c_str());
+				t[i] = '/';
+			}
 		}
-		std::vector<std::basic_string<C>> r;
-		dir_ex(p, r, filter, depth, false);
-		return r;
+		return create_dir(pth);
 	}
-
-	template<typename C>
-	static std::vector<std::basic_string<C>> dir_folders(const C* d, bool (*filter)(const std::basic_string<C>&), int depth = -1)
-	{
-		std::basic_string<C> p(d);
-		if(p.back() != '/')
-		{
-			p.push_back('/');
-		}
-		std::vector<std::basic_string<C>> r;
-		dir_ex(p, r, filter, depth, true);
-		return r;
-	}
-};
-
-bool create_dir(const char* pth)
-{
-	return CreateDirectoryA(pth, NULL) == TRUE;
-}
-bool create_dir(const wchar_t* pth)
-{
-	return CreateDirectoryW(pth, NULL) == TRUE;
-}
-
-template<typename C>
-bool create_dirs(const C* pth)
-{
-	std::basic_string<C> t(pth);
-	for(size_t i = 0; i < t.length(); i++)
-	{
-		if(t[i] =='/' || t[i] == '\\')
-		{
-			t[i] = 0;
-			create_dir(t.c_str());
-			t[i] = '/';
-		}
-	}
-	return create_dir(pth);
 }
--- C++/OS/win/lib/_fls.h
+++ C++/OS/win/lib/_fls.h
@@ -1,36 +1,3 @@
-template<typename S> struct WIN32_FIND_DATA_{};
-template<> struct WIN32_FIND_DATA_<char>{typedef WIN32_FIND_DATAA T;};
-template<> struct WIN32_FIND_DATA_<std::string>{typedef WIN32_FIND_DATAA T;};
-template<> struct WIN32_FIND_DATA_<wchar_t>{typedef WIN32_FIND_DATAW T;};
-template<> struct WIN32_FIND_DATA_<std::wstring>{typedef WIN32_FIND_DATAW T;};
-
-HANDLE FindFirstFile_(LPCSTR filename, LPWIN32_FIND_DATAA ffd)
-{
-	return FindFirstFileA(filename, ffd);
-}
-HANDLE FindFirstFile_(LPCWSTR filename, LPWIN32_FIND_DATAW ffd)
-{
-	return FindFirstFileW(filename, ffd);
-}
-
-BOOL FindNextFile_(HANDLE hff, LPWIN32_FIND_DATAA ffd)
-{
-	return FindNextFileA(hff, ffd);
-}
-BOOL FindNextFile_(HANDLE hff, LPWIN32_FIND_DATAW ffd)
-{
-	return FindNextFileW(hff, ffd);
-}
-
-BOOL DeleteFile_(LPCSTR filename)
-{
-	return DeleteFileA(filename);
-}
-BOOL DeleteFile_(LPCWSTR filename)
-{
-	return DeleteFileW(filename);
-}
-
 BOOL MoveFileEx_(LPCSTR old_path, LPCSTR new_path, DWORD flags)
 {
 	return MoveFileExA(old_path, new_path, flags);
