496320c5e1a8ed0ece1bce563f7ae2ccd22be67e
Alxmhc <Alxmhc@gmail.com>
2025-01-18 19:56:22 +0300

    bint

--- C++/ext/b_int__go_dlib.h
+++ C++/ext/b_int__go_dlib.h
@@ -11,20 +11,38 @@ class b_int_BE
 {
 	dlib lib;
 
+	typedef GoInt(*pMul)(GoSlice a, GoSlice b, GoSlice r);
+	pMul Mul_;
 	typedef GoInt(*pMod)(GoSlice a, GoSlice m);
 	pMod Mod_;
+	typedef GoInt(*pPowMod)(GoSlice a, GoSlice b, GoSlice m);
+	pPowMod PowMod_;
 public:
 	bool Init(const char* path)
 	{
 		if(!lib.Read(path))
 			return false;
+		Mul_ = reinterpret_cast<pMul>(lib.get_addr("Mul"));
+		if(Mul_ == nullptr)
+			return false;
 		Mod_ = reinterpret_cast<pMod>(lib.get_addr("Mod"));
 		if(Mod_ == nullptr)
+			return false;
+		PowMod_ = reinterpret_cast<pPowMod>(lib.get_addr("PowMod"));
+		if(PowMod_ == nullptr)
 			return false;
 		return true;
 	}
 
-	std::vector<uint8_t> Mod(const std::vector<uint8_t> &a, const std::vector<uint8_t> &m)
+	std::vector<uint8_t> Mul(const std::vector<uint8_t> &a, const std::vector<uint8_t> &b) const
+	{
+		std::vector<uint8_t> res(a.size() + b.size());
+		const auto sz = Mul_(GoSlice(const_cast<uint8_t*>(a.data()), a.size()), GoSlice(const_cast<uint8_t*>(b.data()), b.size()), GoSlice(res.data(), res.size()));
+		res.resize(sz);
+		return res;
+	}
+
+	std::vector<uint8_t> Mod(const std::vector<uint8_t> &a, const std::vector<uint8_t> &m) const
 	{
 		std::vector<uint8_t> res;
 		if(a.size() < m.size())
@@ -35,15 +53,16 @@ public:
 		{
 			res = m;
 			const auto sz = Mod_(GoSlice(const_cast<uint8_t*>(a.data()), a.size()), GoSlice(res.data(), res.size()));
-			if(sz != 0)
-			{
-				res.resize(sz);
-			}
-			else
-			{
-				res.assign(1, 0);
-			}
+			res.resize(sz);
 		}
+		return res;
+	}
+
+	std::vector<uint8_t> PowMod(const std::vector<uint8_t> &a, const std::vector<uint8_t> &b, const std::vector<uint8_t> &m) const
+	{
+		std::vector<uint8_t> res = m;
+		const auto sz = PowMod_(GoSlice(const_cast<uint8_t*>(a.data()), a.size()), GoSlice(const_cast<uint8_t*>(b.data()), b.size()), GoSlice(res.data(), res.size()));
+		res.resize(sz);
 		return res;
 	}
 };
--- Go/ext/b_int.go
+++ Go/ext/b_int.go
@@ -5,14 +5,45 @@ import (
 	"math/big"
 )
 
+//export Mul
+func Mul(a, b, res []byte) int {
+	a1 := new(big.Int)
+	a1.SetBytes(a)
+	b1 := new(big.Int)
+	b1.SetBytes(b)
+
+	r1 := new(big.Int)
+	r1.Mul(a1, b1)
+	r := r1.Bytes()
+	copy(res, r)
+	return len(r)
+}
+
 //export Mod
 func Mod(a, m []byte) int {
 	a1 := new(big.Int)
 	a1.SetBytes(a)
 	m1 := new(big.Int)
 	m1.SetBytes(m)
+
 	r1 := new(big.Int)
 	r1.Mod(a1, m1)
+	r := r1.Bytes()
+	copy(m, r)
+	return len(r)
+}
+
+//export PowMod
+func PowMod(a, b, m []byte) int {
+	a1 := new(big.Int)
+	a1.SetBytes(a)
+	b1 := new(big.Int)
+	b1.SetBytes(b)
+	m1 := new(big.Int)
+	m1.SetBytes(m)
+
+	r1 := new(big.Int)
+	r1.Exp(a1, b1, m1)
 	r := r1.Bytes()
 	copy(m, r)
 	return len(r)
