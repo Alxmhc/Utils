66f1144c0b80a412245fba580ec84efe651e2f09
Alxmhc <Alxmhc@gmail.com>
2025-01-24 10:56:54 +0300

    m i

--- C++/ext/b_int__go_dlib.h
+++ C++/ext/b_int__go_dlib.h
@@ -15,6 +15,8 @@ class b_int_BE
 	pMul Mul_;
 	typedef GoInt(*pMod)(GoSlice a, GoSlice m);
 	pMod Mod_;
+	typedef GoInt(*pModInv)(GoSlice a, GoSlice m);
+	pModInv ModInv_;
 	typedef GoInt(*pPowMod)(GoSlice a, GoSlice b, GoSlice m);
 	pPowMod PowMod_;
 public:
@@ -27,6 +29,9 @@ public:
 			return false;
 		Mod_ = reinterpret_cast<pMod>(lib.get_addr("Mod"));
 		if(Mod_ == nullptr)
+			return false;
+		ModInv_ = reinterpret_cast<pModInv>(lib.get_addr("ModInv"));
+		if(ModInv_ == nullptr)
 			return false;
 		PowMod_ = reinterpret_cast<pPowMod>(lib.get_addr("PowMod"));
 		if(PowMod_ == nullptr)
@@ -55,6 +60,14 @@ public:
 			const auto sz = Mod_(GoSlice(const_cast<uint8_t*>(a.data()), a.size()), GoSlice(res.data(), res.size()));
 			res.resize(sz);
 		}
+		return res;
+	}
+
+	std::vector<uint8_t> ModInv(const std::vector<uint8_t> &a, const std::vector<uint8_t> &m) const
+	{
+		std::vector<uint8_t> res = m;
+		const auto sz = ModInv_(GoSlice(const_cast<uint8_t*>(a.data()), a.size()), GoSlice(res.data(), res.size()));
+		res.resize(sz);
 		return res;
 	}
 
--- Go/ext/b_int.go
+++ Go/ext/b_int.go
@@ -33,6 +33,20 @@ func Mod(a, m []byte) int {
 	return len(r)
 }
 
+//export ModInv
+func ModInv(a, m []byte) int {
+	a1 := new(big.Int)
+	a1.SetBytes(a)
+	m1 := new(big.Int)
+	m1.SetBytes(m)
+
+	r1 := new(big.Int)
+	r1.ModInverse(a1, m1)
+	r := r1.Bytes()
+	copy(m, r)
+	return len(r)
+}
+
 //export PowMod
 func PowMod(a, b, m []byte) int {
 	a1 := new(big.Int)
