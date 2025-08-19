0693fe2fe17fc52de52dfeff4185dc241371489d
Alxmhc <Alxmhc@gmail.com>
2025-08-19 22:38:48 +0300

    png

--- Go/ext/png.go
+++ Go/ext/png.go
@@ -4,30 +4,29 @@ import (
 	"C"
 	"_/img"
 	"bytes"
+	"image"
 	"image/png"
 	"os"
 )
 
+func Save_File_PNG(img image.Image, fout string) {
+	fl, err := os.Create(fout)
+	if err == nil {
+		png.Encode(fl, img)
+		fl.Close()
+	}
+}
+
 //export SaveToGr
 func SaveToGr(dat []byte, szx, szy int, fout string) {
-	fl, err := os.Create(fout)
-	if err != nil {
-		return
-	}
-	defer fl.Close()
 	im := img.ReadImgGr(bytes.NewReader(dat), szx, szy)
-	png.Encode(fl, im)
+	Save_File_PNG(im, fout)
 }
 
 //export SaveToRGB
 func SaveToRGB(dat []byte, szx, szy int, fout string) {
-	fl, err := os.Create(fout)
-	if err != nil {
-		return
-	}
-	defer fl.Close()
 	im := img.ReadImgRGB(bytes.NewReader(dat), szx, szy)
-	png.Encode(fl, im)
+	Save_File_PNG(im, fout)
 }
 
 func main() {}
