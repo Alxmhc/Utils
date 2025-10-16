c408744635cb384e29c4eff856e085c3ae4ee9fc
Alxmhc <Alxmhc@gmail.com>
2025-10-16 20:15:07 +0300

    w cert

--- C++/OS/win/-/cert.h
+++ C++/OS/win/-/cert.h
@@ -24,31 +24,41 @@ namespace cert
 			Close();
 		}
 
-		bool Read_System_Storage_User(const char* path)
+		bool Open_System_Storage_User(const char* path)
 		{
 			Close();
-			store = CertOpenStore(CERT_STORE_PROV_SYSTEM_REGISTRY_A, 0, NULL, CERT_STORE_OPEN_EXISTING_FLAG | CERT_STORE_READONLY_FLAG | CERT_SYSTEM_STORE_CURRENT_USER, path);
+			store = CertOpenStore(CERT_STORE_PROV_SYSTEM_A, 0, 0, CERT_STORE_OPEN_EXISTING_FLAG | CERT_SYSTEM_STORE_CURRENT_USER, path);
 			return store != nullptr;
 		}
 
-		bool nxt(PCCERT_CONTEXT &ct)
+		bool Open_File_PEM(const char* fl)
+		{
+			Close();
+			store = CertOpenStore(CERT_STORE_PROV_FILENAME_A, X509_ASN_ENCODING, 0, 0, fl);
+			return store != nullptr;
+		}
+
+		PCCERT_CONTEXT read(PCCERT_CONTEXT ct) const
 		{
 			if(store == nullptr)
-				return false;
-			ct = CertEnumCertificatesInStore(store, ct);
-			return ct != nullptr;
+				return nullptr;
+			return CertEnumCertificatesInStore(store, ct);
 		}
 	};
 
-	static std::string Get_String(PCCERT_CONTEXT ct, DWORD type, DWORD fl)
+	static bool Get_String(PCCERT_CONTEXT ct, DWORD type, DWORD fl, std::string &res)
 	{
-		std::string res;
-		DWORD sz = CertGetNameStringA(ct, type, fl, NULL, NULL, 0);
-		if(sz > 1)
+		const DWORD sz = CertGetNameStringA(ct, type, fl, nullptr, nullptr, 0);
+		if(sz != 0)
 		{
 			res.resize(sz);
-			CertGetNameStringA(ct, type, fl, NULL, &res[0], sz);
+			if(CertGetNameStringA(ct, type, fl, nullptr, &res[0], sz) == sz)
+			{
+				res.resize(sz-1);
+				return true;
+			}
 		}
-		return res;
+		res.clear();
+		return false;
 	}
 }
